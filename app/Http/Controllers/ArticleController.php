27372d0fa28df3a8affb70d996117b858b19c0d7
Vladimir <v.savin@autodrive-agency.ru>
2025-09-05 00:47:46 +0300

    feat(app): spatie html-form

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,9 +8,16 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $articles = Article::paginate();
 
         return view('article.index', compact('articles'));
+    }
+
+    public function show($id)
+    {
+        $article = Article::findOrFail($id);
+        return view('article.show', compact('article'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,6 @@ Route::get('about', [PageController::class, 'about'])->name('about');
 
 Route::get('articles', [ArticleController::class, 'index'])
     ->name('articles.index');
+
+Route::get('articles/{id}', [ArticleController::class, 'show'])
+    ->name('articles.show');
